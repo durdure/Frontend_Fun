4abbf79e5bad55d5b856bf0311463bbcfe8a15a1
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-09 19:38:21 +0300

    fun

---
+++ imageGallery/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <Navbar bg="light" variant="light">
+            {/* Navbar content goes here */}
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ newadd/my-react-app/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import {Navbar} from './components/Header';
+function App() {
+  return (
+   </Navbar>
+  );
+}
+
+export default App;
